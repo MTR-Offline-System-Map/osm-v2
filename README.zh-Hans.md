f3461156ab5a82920a0515115962c716f8b86987
xingteh <gteh@gteh.top>
2025-08-04 22:02:10 +0800

    [U] Fix x2

---
+++ README.zh-Hans.md
@@ -0,0 +1,7 @@
+# OSM 服务端 v2
+
+离线线路图服务端 v2
+
+原版代码: [Minecraft-Transit-Railway/Transport-Simulation-Core](//github.com/Minecraft-Transit-Railway/Transport-Simulation-Core/tree/master/buildSrc/src/main/resources/website)
+
+语言: English (英语), 简体中文
--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 echo "Injecting environment variables..."
 
-cp src/environments/environment.prod.template.ts src/environments/environment.prod.ts
+cp ./src/environments/environment.prod.template.ts ./src/environments/environment.prod.ts
 
-sed -i "s|\$GH_USER|${GH_USER}|g" src/environments/environment.prod.ts
-sed -i "s|\$GH_REPO|${GH_REPO}|g" src/environments/environment.prod.ts
-sed -i "s|\$GH_BRANCH|${GH_BRANCH}|g" src/environments/environment.prod.ts
+sed -i "s|\$GH_USER|${GH_USER}|g" ./src/environments/environment.prod.ts
+sed -i "s|\$GH_REPO|${GH_REPO}|g" ./src/environments/environment.prod.ts
+sed -i "s|\$GH_BRANCH|${GH_BRANCH}|g" ./src/environments/environment.prod.ts
 
 npm run ng build --configuration=production
+
+cp ./_redirects ./dist/osm/browser/
