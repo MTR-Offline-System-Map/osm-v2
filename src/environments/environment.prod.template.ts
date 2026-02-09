8afbc8d6d2fae48285a8cdefc3376b50cdc16747
xingteh <gteh@gteh.top>
2026-02-09 16:01:34 +0800

    fix: translations

--- src/environments/environment.prod.template.ts
+++ src/environments/environment.prod.template.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  dataUrl: (endpoint: string, dimensionIndex: number) => `https://fastly.jsdelivr.net/gh/$GH_USER/$GH_REPO@$GH_BRANCH$GH_PATH/${dimensionIndex}`,
+  dataUrl: (endpoint: string, dimensionIndex: number) => `https://fastly.jsdelivr.net/gh/$GH_USER/$GH_REPO@$GH_BRANCH$GH_PATH${dimensionIndex}`,
   worldMapLink: {
     enable: (dimensionIndex: number) => false,
     exec: (dimensionIndex: number, x: number, z: number) => ``,
