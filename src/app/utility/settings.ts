2c7eba905596aed58b574817e68e0985625b5949
xingteh <gteh@gteh.top>
2026-03-17 22:50:24 +0800

    chore: forgot to commit this file

--- src/app/utility/settings.ts
+++ src/app/utility/settings.ts
@@ -1,8 +1,28 @@
-const SETTINGS = {
+import {LineMaterial} from "three/examples/jsm/lines/LineMaterial.js";
+
+export const SETTINGS = {
 	scale: 1,
 	maxText: 16,
 	maxBlobs: 128,
 	maxVertices: 99999,
 };
 
-export default SETTINGS;
+export const CONNECTIONS = {
+	lineMaterialStationConnectionThin: new LineMaterial({color: 0xFFFFFF, linewidth: 4 * SETTINGS.scale * devicePixelRatio, vertexColors: true}),
+	lineMaterialStationConnectionThick: new LineMaterial({color: 0xFFFFFF, linewidth: 8 * SETTINGS.scale * devicePixelRatio, vertexColors: true}),
+	lineMaterialNormal: new LineMaterial({color: 0xFFFFFF, linewidth: 6 * SETTINGS.scale * devicePixelRatio, vertexColors: true}),
+	lineMaterialNormalDashed: new LineMaterial({color: 0xFFFFFF, linewidth: 6 * SETTINGS.scale * devicePixelRatio, vertexColors: true, dashed: true}),
+	lineMaterialThin: new LineMaterial({color: 0xFFFFFF, linewidth: 3 * SETTINGS.scale * devicePixelRatio, vertexColors: true}),
+	lineMaterialThinDashed: new LineMaterial({color: 0xFFFFFF, linewidth: 3 * SETTINGS.scale * devicePixelRatio, vertexColors: true, dashed: true}),
+};
+
+
+export function refreshMapScale(scale: number) {
+	SETTINGS.scale = scale;
+	CONNECTIONS.lineMaterialStationConnectionThin = new LineMaterial({color: 0xFFFFFF, linewidth: 4 * scale * devicePixelRatio, vertexColors: true});
+	CONNECTIONS.lineMaterialStationConnectionThick = new LineMaterial({color: 0xFFFFFF, linewidth: 8 * scale * devicePixelRatio, vertexColors: true});
+	CONNECTIONS.lineMaterialNormal = new LineMaterial({color: 0xFFFFFF, linewidth: 6 * scale * devicePixelRatio, vertexColors: true});
+	CONNECTIONS.lineMaterialNormalDashed = new LineMaterial({color: 0xFFFFFF, linewidth: 6 * scale * devicePixelRatio, vertexColors: true, dashed: true});
+	CONNECTIONS.lineMaterialThin = new LineMaterial({color: 0xFFFFFF, linewidth: 3 * scale * devicePixelRatio, vertexColors: true});
+	CONNECTIONS.lineMaterialThinDashed = new LineMaterial({color: 0xFFFFFF, linewidth: 3 * scale * devicePixelRatio, vertexColors: true, dashed: true});
+}
