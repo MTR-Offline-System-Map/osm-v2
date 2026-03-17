8f235a5a6c60765d1f0c4b30355da511d161ea77
xingteh <gteh@gteh.top>
2026-03-17 22:41:56 +0800

    feat: minimized drawer

--- src/app/component/drawer/drawer.component.ts
+++ src/app/component/drawer/drawer.component.ts
@@ -15,27 +15,28 @@ import {ButtonModule} from "primeng/button";
 })
 export class DrawerComponent {
 	protected drawerVisible = false;
+	protected drawerMinimized = false;
 	protected drawerPosition: "bottom" | "right" = "right";
 	protected drawerStyle = {};
-	protected drawerClass = "";
+	protected readonly drawerStyleMinimized = {width: "fit-content", height: "fit-content"};
 	@Input({required: true}) title = "";
 	@Output() closed = new EventEmitter<void>;
 
 	constructor() {
-		window.addEventListener("resize", () => this.resize());
-		this.resize();
+		screen.orientation.addEventListener("change", () => this.resize(screen.orientation.type.startsWith("portrait")));
+		this.resize(screen.orientation.type.startsWith("portrait"));
 	}
 
 	open() {
 		this.drawerVisible = true;
+		this.drawerMinimized = false;
 	}
 
 	close() {
 		this.drawerVisible = false;
 	}
 
-	private resize() {
-		const vertical = window.innerWidth < window.innerHeight;
+	private resize(vertical: boolean) {
 		this.drawerPosition = vertical ? "bottom" : "right";
 		this.drawerStyle = vertical ? {height: "48rem", maxHeight: "80%"} : {width: "24rem", maxWidth: "80%"};
 	}
